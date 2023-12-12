61548582dbf8f59884a2726b50c2601abc459a9d
jonathafernandes <jonathafernandes@live.com>
2023-12-12 10:47:11 -0300

    change layout and remove habits

--- README.md
+++ README.md
@@ -1,40 +1,16 @@
-<h1 align="center"> Habits & Goals </h1>
+<h1 align="center">Hábitos</h1>
 
 <p align="center">
-Controle de hábitos e metas.
+Controle de hábitos.
 <br>
-</p>
-
-<p align="center">
-  <a href="#-tecnologias">Tecnologias</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
-  <a href="#-projeto">Projeto</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
 </p>
 
 <br>
 
-<p align="center">
-  <img alt="" src="./assets/layout.png" width="60%">
-  <br><br>
-  <img alt="" src="./assets/layout-1.png" width="60%">
-</p>
-
-## 🚀 Tecnologias
-
-Esse projeto foi desenvolvido com as seguintes tecnologias:
-
+🚀 **Desenvolvido com:**
 - HTML e CSS
 - JavaScript
 - Git e Github
-
-## 💻 Projeto
-
-Aplicativo de controle de hábitos e metas.
-
-## 🖱️ Visite
-
-https://jonathafernandes.github.io/habits-e-goals/
-
 ---
-👨‍💻 Desenvolvedor
-<br>
-- Jonatha Fernandes
+**🖱️ Visite:**
+- https://jonathafernandes.github.io/habits-e-goals/
--- src/script.js
+++ src/script.js
@@ -1,6 +1,6 @@
 const form = document.querySelector('#form-habits')
-const nlwSetup = new NLWSetup(form)
-const button = document.querySelector("header button")
+const app = new NLWSetup(form)
+const button = document.querySelector(".button")
 
 button.addEventListener("click", add)
 form.addEventListener("change", save)
@@ -8,22 +8,22 @@ form.addEventListener("change", save)
 function add() {
 
   const today = new Date().toLocaleDateString('pt-br').slice(0, -5)
-  const dayExists = nlwSetup.dayExists(today)
+  const dayExists = app.dayExists(today)
 
   if (dayExists) {
     alert("O dia de hoje já foi adicionado!")
     return
   }
 
-  alert("Dia adicionado com sucesso! ✅")
-  nlwSetup.addDay(today)
+  alert("Dia adicionado com sucesso!")
+  app.addDay(today)
 }
 
 function save() {
-  localStorage.setItem('NLWSetup@habits', JSON.stringify(nlwSetup.data)) // Usando JSON para transformar os dados de objeto para string
+  localStorage.setItem('NLWSetup@habits', JSON.stringify(app.data)) // Usando JSON para transformar os dados de objeto para string
 }
 
 const data = JSON.parse(localStorage.getItem('NLWSetup@habits')) || {} // Usando JSON para trasnformas dados de string para objeto
 
-nlwSetup.setData(data)
-nlwSetup.load()
+app.setData(data)
+app.load()
